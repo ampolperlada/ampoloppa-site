ed6eccd5d1ba8953f3a5f80b4659a406351f2988
Christian Perlada <74958847+ampolperlada@users.noreply.github.com>
2024-11-24 21:36:39 +0800

    educ body

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -11,12 +11,12 @@ const Education = () => {
         <div class="timeline-item">
           <div class="timeline-dot"></div>
 
-            <div class="timeline-date">2021</div>
+            <div class="timeline-date">2018</div>
             <div class="timeline-content">
               <h3>High School</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
+              <p>Completed secondary education at Captain Albert Aguilar National High School, 
+                where I built a strong foundation in math and science that 
+                inspired my interest in technology and problem-solving.
                </p>
             </div>
         </div>
@@ -24,12 +24,11 @@ const Education = () => {
         
         <div class="timeline-item">
           <div class="timeline-dot"></div>
-            <div class="timeline-date">2022</div>
+            <div class="timeline-date">2020</div>
             <div class="timeline-content">
-              <h3>University</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
+              <h3>Senior High</h3>
+              <p>
+              Completed Senior High School at Southville International School in Las Piñas, specializing in ICT. This experience built a strong foundation in technology and digital skills, sparking my passion for innovation and problem-solving.
              </p>
             </div>
       
@@ -38,11 +37,11 @@ const Education = () => {
         
         <div class="timeline-item">
           <div class="timeline-dot"></div>
-            <div class="timeline-date">2023</div>
+            <div class="timeline-date">2022</div>
             <div class="timeline-content">
               <h3>College</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+              <p>
+              Graduated with an Associate Degree from Southville International School under the ASAT program, completing two years of college in Information System Technology (IST) and Information System Management (ISM). Achieved Dean’s List honors and received the Best Research Award and Best System Proposal, showcasing my dedication to academic excellence and innovative problem-solving
              </p>
             </div>
           
@@ -52,11 +51,8 @@ const Education = () => {
           <div class="timeline-dot"></div>
             <div class="timeline-date">2024</div>
             <div class="timeline-content">
-              <h3>Internship</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                eprehenderit in voluptate 
-                velit esse cillum dolore eu fugiat nulla pariatur.</p>
+              <h3>College</h3>
+              <p>After earning my Associate Degree from Southville International School, I transferred to Saint Bernadette College of Alabang to continue my studies and graduated with a Bachelor’s Degree in Information Technology (BSIT), prio on Software Engineering. This allowed me to deepen my expertise in software development and advance my technical skills.</p>
             </div>
           
         </div>
