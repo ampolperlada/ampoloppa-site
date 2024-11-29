13e87c675d68ac78e49d2c1c6dfb048056ee4917
Christian Perlada <74958847+ampolperlada@users.noreply.github.com>
2024-11-29 17:07:48 +0800

    access-key-email 1

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -3,6 +3,29 @@ import './ContactForm.css'
 
 const ContactForm = () => {
   return (
+    const onSubmit = async (event) => {
+      event.preventDefault();
+      const formData = new FormData(event.target);
+  
+      formData.append("access_key", "15e3e590-9f9e-4dbf-88d1-53e9b6e92433");
+  
+      const object = Object.fromEntries(formData);
+      const json = JSON.stringify(object);
+  
+      const res = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        },
+        body: json
+      }).then((res) => res.json());
+  
+      if (res.success) {
+        console.log("Success", res);
+      }
+    };
+
     <div className='contact-form-content'>
       <form>
         <div className='name-container'>
