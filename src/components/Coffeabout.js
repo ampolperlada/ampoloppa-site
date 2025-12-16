9682c69271ea2a959c4b0cf218263487950c83ef
Christian Perlada <74958847+ampolperlada@users.noreply.github.com>
2025-12-16 17:15:17 +0800

    Update Coffeabout.js

--- src/components/Coffeabout.js
+++ src/components/Coffeabout.js
@@ -19,7 +19,7 @@ const Coffeabout = () => {
     applying your knowledge of HTML, CSS, and JavaScript.</p>
 
 
-    <a href="/Cv/Resume(99).pdf" class="btn effect04" data-sm-link-text="SV" download target="_blank"><span>Download</span></a>
+    <a href="/Cv/Resume(X).pdf" class="btn effect04" data-sm-link-text="SV" download target="_blank"><span>Download</span></a>
 
 
         </div>
