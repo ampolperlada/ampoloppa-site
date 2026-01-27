ce286df3f38c837fd7a4fc473db99a0b99c5e9e8
Christian Perlada <74958847+ampolperlada@users.noreply.github.com>
2026-01-27 22:11:31 +0800

    Refactor Workexp component: remove slider functionality, enhance layout, and improve heading styling

--- src/components/Workexp.jsx
+++ src/components/Workexp.jsx
@@ -1,66 +1,26 @@
-import React from 'react'
-import './Workexp.css'
-import { WORK_EXPERIENCE } from '../utils/data'
+import React from 'react';
+import './Workexp.css';
+import { WORK_EXPERIENCE } from '../utils/data';
 import Expcard from './Expcard';
-import { useRef } from 'react';
-import Slider from 'react-slick';
-
-
 
 const Workexp = () => {
-    const sliderRef = useRef();
-  const settings = {
-    arrows: false, // Disables arrows
-    dots: false,   // Disables dots
-    infinite:true,
-    speed: 500,
-    slidesToScroll: 2,
-    slidesToShow: 2,
-    arros: false,
-    responsible: [
-      {
-        breakpoint: 769,
-        settings:{
-          slidesToScroll: 1,
-          slidesToShow: 1,
-        },
-      },
-    ],
-  };
-
-  const slideRight = () => {
-    sliderRef.current.slickNext();
-  };
-
-  const sliderLeft = () => {
-    sliderRef.current.slickPrev();
-  };
-
-
-  
   return (
     <section className='experience-container'>
-      <h5> Certifications </h5> 
+      <h5 className='certifications-heading'>
+        <span className='heading-bracket'>&lt;</span>
+        Certifications
+        <span className='heading-bracket'>/&gt;</span>
+      </h5>
 
-      <div className='experience-content'> 
-      
-      <div className='arrow-right' onClick={slideRight}>
-        <span className="material-icons">arrow_forward</span>
-      </div>
-
-      <div className='arrow-left' onClick={sliderLeft}>
-        <span className="material-icons">arrow_back</span>
-      </div>
-
-
-        <Slider ref={sliderRef} {...settings}>
-      {WORK_EXPERIENCE.map((item)=> (
-        <Expcard key={item.title} details={item} />
-      ))}
-  </Slider>
+      <div className='experience-content'>
+        <div className='certifications-grid'>
+          {WORK_EXPERIENCE.map((item) => (
+            <Expcard key={item.title} details={item} />
+          ))}
+        </div>
       </div>
     </section>
   );
 };
 
-export default Workexp
+export default Workexp;
